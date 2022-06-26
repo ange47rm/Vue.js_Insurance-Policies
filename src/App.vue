fca1f528c7e963b297f3cec5e2a47458d5648777
ange47rm <ange47rm@hotmail.com>
2022-06-26 18:34:58 +0100

    Changes after adding Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,46 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="amber darken-3" dark>
+      <h1>Insurance Policies - Broker Insights</h1>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <InsurancePolicyHome v-bind:client="client" />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import InsurancePolicyHome from './components/InsurancePolicyHome.vue';
+import Client from './models/Client.js'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    InsurancePolicyHome
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      client: new Client()
+    }
+  },
+
+  mounted() {
+    fetch("./achme-broker.json")
+      .then(res => res.json())
+      .then(data => {
+        this.client.name = data.client.name;
+        this.client.policies = data.client.policies;
+      });
+  }
 };
 </script>
+
+<style scoped>
+body {
+  text-align: center !important;
+}
+</style>
